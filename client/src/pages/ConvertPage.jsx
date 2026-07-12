cb7d4ded573ffa0c6b07adb537f839532344d9a6
Hailey Martin <101282320+Hailo7ts@users.noreply.github.com>
2026-07-12 14:01:19 -0600

    distance function not working outside sw brand

--- client/src/pages/ConvertPage.jsx
+++ client/src/pages/ConvertPage.jsx
@@ -40,21 +40,32 @@ const ConvertPage = () => {
 
   };
 
-  //function to convert hex to rgb 
-  function getRGB(hex){
+  //function to convert hex/rgb to formatted object rgb 
+  function getRGB(digitalColor) {
 		
+    
     let rgb = {
       r: 0,
       g: 0,
       b: 0  
     }
 		
-    const cleaned = hex.replace(/^#/, '');
+    //check if color is hex or rgb and convert to rgb object
+    if (!digitalColor.startsWith('rgb')) {
+      //color is hex remove # and format to rgb object
+      const cleaned = digitalColor.replace(/^#/, '');
     
-    rgb.r = parseInt(cleaned.substring(0, 2), 16);
-    rgb.g = parseInt(cleaned.substring(2, 4), 16);
-    rgb.b = parseInt(cleaned.substring(4, 6), 16);
-    
+      rgb.r = parseInt(cleaned.substring(0, 2), 16);
+      rgb.g = parseInt(cleaned.substring(2, 4), 16);
+      rgb.b = parseInt(cleaned.substring(4, 6), 16);
+    }else{
+      //color is rgb format remove rgb() and format to rgb object
+      rgb.r = parseInt(digitalColor.slice(4, 7))
+      rgb.g = parseInt(digitalColor.slice(9, 12))
+      rgb.b = parseInt(digitalColor.slice(14, 17))
+    }
+
+    //return digital color as rgb object
     return rgb;
 	}
 	 
@@ -65,10 +76,12 @@ const ConvertPage = () => {
     let minDistance = Infinity
     let closestColor = null
 
+
     //loop through brand arrays rgb values
     for (const colorHex of arrOfBrands){
       //console.log('Processing color:', colorHex)
       let currentColor = colorHex
+      //console.log('Current color:', currentColor.rgb)
 
       //euclidean distance formula to find closest match without using sqrt to save processing time
       let distance = 
@@ -76,7 +89,8 @@ const ConvertPage = () => {
            Math.pow((targetColor.g - currentColor.rgb.g), 2) +
            Math.pow((targetColor.b - currentColor.rgb.b), 2)
       
-    
+    //FLAG `````````````````````````````````````````````````````````````````````````````
+           //console.log(currentColor.rgb.r)
       //compare distance to minDistance if less than minDistance set minDistance to distance and closestColor to currentColor
       if(distance < minDistance){
         minDistance = distance
@@ -99,32 +113,55 @@ const ConvertPage = () => {
 	    .then(data => {
         console.log(data)
         let colorBook = data
-        let rgbConvertedColor = ''
-        //convert color to be rgb
-			  rgbConvertedColor = getRGB(color.colorCode)
-			 
 
-      //check if brand uses hex if it does convert to rgb
-      if(colorBook[0].hex.startsWith('#')){
-        colorBook.map(colorBookColor => {
+        //check if brand uses hex if it does convert to rgb
+        if(colorBook[0].hex.startsWith('#')){
           
-          //convert color from hex to rgb add to brand array
-          let convertedColor = getRGB(colorBookColor.hex)
-          colorBookColor['rgb'] = convertedColor
-          
-          //add to brand array
-          brandArray.push(colorBookColor)
-          
-        })}
+          //convert color to be rgb
+			    let rgbConvertedColor = getRGB(color.colorCode)
+          console.log(rgbConvertedColor)
 
-      console.log(brandArray)
+      
+          colorBook.map(colorBookColor => {
+          
+            //convert color from hex to rgb add to brand array
+            let convertedColor = getRGB(colorBookColor.hex)
+            
+            colorBookColor['rgb'] = {r: convertedColor.r, g: convertedColor.g, b: convertedColor.b}
+          console.log(colorBookColor.rgb)
+            //add to brand array
+            brandArray.push(colorBookColor)
+          
+          })
 
-      //call 3d euclidean distance function to find closest match
-      let closestMatch = euclideanDistance(rgbConvertedColor, brandArray)
-      console.log(closestMatch)
+          console.log(brandArray)
+
+          //call 3d euclidean distance function to find closest match
+          let closestMatch = euclideanDistance(rgbConvertedColor, brandArray)
+          console.log(closestMatch)
 
     
-	    })
+	    } else{        
+
+        colorBook.map(colorBookColor => {
+          colorBookColor['rgb'] = {r: colorBookColor.hex.r, g: colorBookColor.hex.g, b: colorBookColor.hex.b}
+            //add to brand array
+            brandArray.push(colorBookColor)
+          
+          })
+
+         let targetColor = {}
+          //convert color to be rgb
+			    targetColor = getRGB(color.colorCode)
+
+          console.log(targetColor)
+
+          //call 3d euclidean distance function to find closest match
+          let closestMatch = euclideanDistance(targetColor, brandArray)
+          console.log(closestMatch)
+
+        }
+  })
 
       .catch(err => console.log(`Error in fetch: ${err}`))
     
@@ -190,8 +227,19 @@ const ConvertPage = () => {
                       </select>
                     </label>
 
-                    <label className="label">Color Value Type
-                      <input type="textarea" name="colorCode" placeholder="color code" className="input input-bordered" onChange={handleColorChange} required />
+                    {/* Color Code Input */}
+                    <label className="label">Your Color Code
+                      {/* check if value type is rgb or hex */}
+                      {colorType === "hex" &&
+                        <input type="textarea" name="colorCode" placeholder="#000000" className="input input-bordered" onChange={handleColorChange} required />
+                      } 
+                      
+                      {
+                      colorType === "rgb" &&
+                        <input type="textarea" name="colorCode" placeholder="rgb(255, 255, 255)" className="input input-bordered" onChange={handleColorChange} required />
+                      }
+                      
+                      
                     </label>
 
                     <button type="submit" className="btn btn-warning" >CONVERT</button>
